c8e2ce6d3b1c6abc5d36c5a5aec526b78b1191f5
kevin4183 <anwannandikenneth@gmail.com>
2022-11-13 12:48:29 +0100

    Upadte

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 5050;
+const port = process.env.PORT || 5050;
 //loads data from env variable
-require("dotenv").config();
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users");
-const postRoute = require("./routes/posts");
-const categoryRoute = require("./routes/categories");
-const multer = require("multer");
-const path = require("path");
+require('dotenv').config();
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
+const multer = require('multer');
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use('/images', express.static(path.join(__dirname, '/images')));
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 // mongoose
 //   .connect(process.env.MONGODB_URL, {
 //     useNewUrlParser: true,
@@ -27,16 +27,16 @@ const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGODB_URL, function (err) {
   if (err) {
-    console.error("Failed to connect successfully");
+    console.error('Failed to connect successfully');
   } else {
-    console.log("Database connected successfully");
+    console.log('Database connected successfully');
   }
 });
 
 // for uploading images
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "images");
+    cb(null, 'images');
   },
   filename: (req, file, cb) => {
     // req.body.name sends the image to reaactjs(frontend)
@@ -45,15 +45,15 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaed");
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaed');
 });
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/categories", categoryRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
+app.use('/api/categories', categoryRoute);
 
 app.listen(port, () => {
-  console.log("Backend is running on", port);
+  console.log('Backend is running on', port);
 });
